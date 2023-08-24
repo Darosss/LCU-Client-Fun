fcfd8d6edd8f7fa16def36655ace445331372667
Darosss <daroing12@gmail.com>
2023-08-24 19:53:32 +0200

    fix: replace ChampSelect to None in initialLCUContextValue.currentPhase

--- src/LCU/lcucontext.tsx
+++ src/LCU/lcucontext.tsx
@@ -29,7 +29,7 @@ export const initialLCUContextValue: LCUContext = {
     autoAccept: false,
   },
   changeOptions: () => {},
-  currentPhase: "ChampSelect",
+  currentPhase: "None",
   setCurrentSummoner: () => {},
   setCurrentPhase: () => {},
   changeCurrentLobbyConfig: () => {},
