bffae11aa8e571fe38f3549a7d8b5dc15ba86847
Darosss <daroing12@gmail.com>
2023-08-24 19:41:07 +0200

    feat: add spellsfetch functions

---
+++ src/helpers/spellsfetch.ts
@@ -0,0 +1,51 @@
+import * as path from "path";
+import {
+  CURRENT_LOL_VERSION,
+  ddragonLeagueOfLegendsBaseLink,
+} from "../globals";
+import { getData } from "./fetchdata";
+import { writeData, dataFileExists, readData } from "./fsdata";
+import {
+  DataDragonSpellsDataResponse,
+  DataDragonSpellsJsonFileData,
+} from "../LCU/types";
+
+const spellsFilePath = path.join(
+  __dirname,
+  `spells${CURRENT_LOL_VERSION.trim()}.json`
+);
+
+export async function getDataDragonSpells(): Promise<DataDragonSpellsDataResponse> {
+  const spellsResponse = await getData(
+    ddragonLeagueOfLegendsBaseLink("summoner.json")
+  );
+
+  return spellsResponse as DataDragonSpellsDataResponse;
+}
+
+export function writeDragonSpellsData(value: DataDragonSpellsJsonFileData[]) {
+  writeData<DataDragonSpellsJsonFileData[]>(spellsFilePath, value);
+}
+
+async function ifDataDragonSpellsDoesNotExist() {
+  if (!dataFileExists(spellsFilePath)) {
+    const spellsDragonData: DataDragonSpellsJsonFileData[] = [];
+    const { data: responseData } = await getDataDragonSpells();
+    for (const [, value] of Object.entries(responseData)) {
+      spellsDragonData.push({
+        name: value.name,
+        id: parseInt(value.key, 10),
+        modes: value.modes,
+        summonerLevel: value.summonerLevel,
+      });
+    }
+
+    writeDragonSpellsData(spellsDragonData);
+  }
+}
+
+export async function readDragonSpellsData() {
+  await ifDataDragonSpellsDoesNotExist();
+
+  return readData<DataDragonSpellsJsonFileData[]>(spellsFilePath);
+}
