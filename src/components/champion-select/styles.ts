8e2485e244cf98a1dc6e75c36d995899a255b0fb
Darosss <daroing12@gmail.com>
2023-08-29 18:29:30 +0200

    refactor: remove un used styles.ts

--- src/components/champion-select/styles.ts
+++
@@ -1,6 +0,0 @@
-import { backgroundLinearGradient } from "../styles";
-
-export const disabledChampionBtn = `
-    color:red;
-    ${backgroundLinearGradient(`rgba(12,31,44,1)`, `rgba(12,123,12,0.7)`)} 
-`;
