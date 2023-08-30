b5352c2ada62af0841545f5c8946a7c6b37af699
Darosss <daroing12@gmail.com>
2023-08-30 23:56:45 +0200

    feat: add usage of AutoChampionPick + wrap options in Tabs instead of only View

--- src/components/phaseview/sidebar/options.tsx
+++ src/components/phaseview/sidebar/options.tsx
@@ -1,6 +1,14 @@
 import React, { useContext, useState } from "react";
-import { Button, LineEdit, Text, View } from "@nodegui/react-nodegui";
+import {
+  Button,
+  LineEdit,
+  TabItem,
+  Tabs,
+  Text,
+  View,
+} from "@nodegui/react-nodegui";
 import { LCUContext } from "../../../LCU/lcucontext";
+import { AutoChampionPick } from "./auto-champion-pick";
 
 export function Options() {
   const {
@@ -12,51 +20,60 @@ export function Options() {
     height: number;
   }>({ width: minSize.width, height: minSize.height });
   return (
-    <View id="sidebar-options">
-      <Button
-        id={autoAccept ? `button-enabled` : `button-disabled`}
-        text={`Auto accept:  ${autoAccept}`}
-        on={{
-          clicked: () => changeOptions({ autoAccept: !autoAccept }),
-        }}
-      ></Button>
-      <View>
-        <Text>Client width:</Text>
-        <LineEdit
-          text={`${localSizesWindow.width}`}
-          on={{
-            textChanged: (value) => {
-              if (isNaN(Number(value))) return;
-              setLocalSizesWindow((prevState) => ({
-                ...prevState,
-                width: Number(value),
-              }));
-            },
-          }}
-        />
-        <Text>Client height:</Text>
-        <LineEdit
-          text={`${localSizesWindow.height}`}
-          on={{
-            textChanged: (value) => {
-              if (isNaN(Number(value))) return;
-              setLocalSizesWindow((prevState) => ({
-                ...prevState,
-                height: Number(value),
-              }));
-            },
-          }}
-        />
-        <Button
-          on={{
-            clicked: () => {
-              changeOptions({ minSize: localSizesWindow });
-            },
-          }}
-          id="button-default"
-          text="Apply"
-        />
-      </View>
-    </View>
+    <Tabs>
+      <TabItem title="general">
+        <View>
+          <Button
+            id={autoAccept ? `button-enabled` : `button-disabled`}
+            text={`Auto accept:  ${autoAccept}`}
+            on={{
+              clicked: () => changeOptions({ autoAccept: !autoAccept }),
+            }}
+          ></Button>
+          <View>
+            <Text>Client width:</Text>
+            <LineEdit
+              text={`${localSizesWindow.width}`}
+              on={{
+                textChanged: (value) => {
+                  if (isNaN(Number(value))) return;
+                  setLocalSizesWindow((prevState) => ({
+                    ...prevState,
+                    width: Number(value),
+                  }));
+                },
+              }}
+            />
+            <Text>Client height:</Text>
+            <LineEdit
+              text={`${localSizesWindow.height}`}
+              on={{
+                textChanged: (value) => {
+                  if (isNaN(Number(value))) return;
+                  setLocalSizesWindow((prevState) => ({
+                    ...prevState,
+                    height: Number(value),
+                  }));
+                },
+              }}
+            />
+            <Button
+              on={{
+                clicked: () => {
+                  changeOptions({ minSize: localSizesWindow });
+                },
+              }}
+              id="button-default"
+              text="Apply"
+            />
+          </View>
+        </View>
+      </TabItem>
+      <TabItem title="auto-accept">
+        <View id="auto-champion-pick-wrapper">
+          <AutoChampionPick />
+        </View>
+      </TabItem>
+    </Tabs>
   );
 }
