cb923fb1da98a8271e00b0ee4d5c1dbd35a72716
Darosss <daroing12@gmail.com>
2023-08-30 23:57:21 +0200

    feat: add AutoChampionPick components

---
+++ src/components/phaseview/sidebar/auto-champion-pick.tsx
@@ -0,0 +1,110 @@
+import { View, Text, LineEdit, Button } from "@nodegui/react-nodegui";
+import React, { useContext, useState } from "react";
+import { LCUContext } from "../../../LCU/lcucontext";
+import { AssignedPosition } from "../../../LCU/types";
+import { getPercentFromValue } from "../../../helpers/node-gui-responsive-helpers";
+import { AutoChampionChampionsList } from "./auto-champion-champions-list";
+
+const assignedPositions: AssignedPosition[] = [
+  "utility",
+  "middle",
+  "top",
+  "bottom",
+  "jungle",
+];
+
+export function AutoChampionPick() {
+  const {
+    options: {
+      autoPickChamps,
+      minSize: { height },
+    },
+  } = useContext(LCUContext);
+  const [showMoreAssignedChampions, setShowMoreAssignedChampions] =
+    useState(false);
+  const [currentChoosenPosition, setCurrentPositionToAdd] =
+    useState<AssignedPosition>("utility");
+
+  const [champFilter, setChampFilter] = useState<string | null>(null);
+
+  function showAssignedChampionsToPositions() {
+    return assignedPositions.map((position, idx) => (
+      <View id="assigned-champions-to-position" key={idx}>
+        <Button
+          id="assigned-champions-to-position-header"
+          style={`${
+            currentChoosenPosition === position
+              ? "border:1px solid green; color:green;"
+              : ""
+          }`}
+          text={position}
+          on={{
+            clicked: () => {
+              setCurrentPositionToAdd(position);
+            },
+          }}
+        />
+        <View>
+          {autoPickChamps?.[position].map((champ, idx) => (
+            <Text
+              key={idx}
+              on={{
+                MouseButtonPress: () => {
+                  setChampFilter(champ.name);
+                },
+              }}
+              style={`${
+                champFilter && champ.name.toLowerCase().includes(champFilter)
+                  ? "color:red;"
+                  : ""
+              }`}
+            >
+              {champ.name}
+            </Text>
+          ))}
+        </View>
+      </View>
+    ));
+  }
+
+  return (
+    <>
+      <View>
+        <Text id="button-default"> Auto champion pick </Text>
+        <Button
+          text={`${showMoreAssignedChampions ? "Show less" : "Show more"}`}
+          id="button-default"
+          on={{
+            clicked: () => {
+              setShowMoreAssignedChampions(!showMoreAssignedChampions);
+            },
+          }}
+        />
+      </View>
+      <View id="auto-champion-pick-search-wrapper">
+        <Text id="button-default"> Search champion </Text>
+        <LineEdit
+          text={champFilter || ""}
+          on={{
+            textChanged: (e) => setChampFilter(e.toLowerCase()),
+          }}
+        />
+      </View>
+      <View
+        id="auto-champion-roles-wrapper"
+        style={`${
+          showMoreAssignedChampions
+            ? `max-height:${getPercentFromValue(height, 80)}`
+            : `max-height:${getPercentFromValue(height, 15)}`
+        }`}
+      >
+        {showAssignedChampionsToPositions()}
+      </View>
+
+      <AutoChampionChampionsList
+        currentChoosenPosition={currentChoosenPosition}
+        champFilter={champFilter}
+      />
+    </>
+  );
+}
