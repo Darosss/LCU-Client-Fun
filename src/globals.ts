e63e39a62a3677b3499651739f8c1d9af5020a4d
Darosss <daroing12@gmail.com>
2023-08-30 12:53:01 +0200

    version: change version to current version of lol

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,4 @@
-export const CURRENT_LOL_VERSION = "13.16.1";
+export const CURRENT_LOL_VERSION = "13.17.1";
 //TODO: move to env later or sth
 export function ddragonLeagueOfLegendsBaseLink(
   suffixUrl: string,
