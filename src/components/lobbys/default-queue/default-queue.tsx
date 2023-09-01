e45252c987d70bcd8e56f6353d685822ed82397a
Darosss <daroing12@gmail.com>
2023-09-01 23:56:51 +0200

    feat: add usage of PositionSelector + LobbyMembers + add conditional rendering

--- src/components/lobbys/default-queue/default-queue.tsx
+++ src/components/lobbys/default-queue/default-queue.tsx
@@ -1,9 +1,23 @@
 import React, { useContext } from "react";
 import { LCUContext } from "../../../LCU/lcucontext";
 import { SearchMatchBtn } from "./search-match-btn";
+import { View } from "@nodegui/react-nodegui";
+import { PositionSelector } from "./position-selector";
+import { LobbyMembers } from "./lobby-members";
 
 export function DefaultQueue() {
   const { lobbyData } = useContext(LCUContext);
-
-  return lobbyData?.localMember.isLeader ? <SearchMatchBtn /> : null;
+  if (!lobbyData) return null;
+  return (
+    <View
+      id="default-queue-wrapper"
+      //FIXME: i don't know styles from stylesheet with display didnt work there
+      // so i put inline style here
+      style="display:'flex';flex-direction:'column';"
+    >
+      {lobbyData.gameConfig.showPositionSelector ? <PositionSelector /> : null}
+      {lobbyData.localMember.isLeader ? <SearchMatchBtn /> : null}
+      <LobbyMembers />
+    </View>
+  );
 }
