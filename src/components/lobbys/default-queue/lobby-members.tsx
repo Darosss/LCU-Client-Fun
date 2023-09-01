846e7c6376c58658e15cdb3bd271258ed9450568
Darosss <daroing12@gmail.com>
2023-09-02 00:11:10 +0200

    feat: add conditional rendering for secondary preference position

--- src/components/lobbys/default-queue/lobby-members.tsx
+++ src/components/lobbys/default-queue/lobby-members.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "@nodegui/react-nodegui";
 import React, { useContext } from "react";
 import { LCUContext } from "../../../LCU/lcucontext";
+import { PositionsPreferences } from "../../../LCU/types";
 
 export function LobbyMembers() {
   const { lobbyData } = useContext(LCUContext);
@@ -17,7 +18,9 @@ export function LobbyMembers() {
             <Text> {summonerName} </Text>
             <View id="lobby-one-member-position-preferences-wrapper">
               <Text> {firstPositionPreference}</Text>
-              <Text> {secondPositionPreference}</Text>
+              {firstPositionPreference !== PositionsPreferences.FILL ? (
+                <Text> {secondPositionPreference}</Text>
+              ) : null}
             </View>
           </View>
         )
