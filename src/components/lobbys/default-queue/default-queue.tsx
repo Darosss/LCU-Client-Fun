f6327738797bf2d85e4ef440974a568ac7f4e777
Darosss <daroing12@gmail.com>
2023-09-01 11:05:36 +0200

    refactor: remove useEventHandler and make a normal function searchMatch

--- src/components/lobbys/default-queue/default-queue.tsx
+++ src/components/lobbys/default-queue/default-queue.tsx
@@ -1,25 +1,25 @@
-import { Button, useEventHandler } from "@nodegui/react-nodegui";
+import { Button } from "@nodegui/react-nodegui";
 import { playBtn } from "../styles";
-import { QPushButtonSignals } from "@nodegui/nodegui";
 import React from "react";
 import { lcuClientHandlerObj } from "../../../LCU/LCUClientHandler";
 
 export function DefaultQueue() {
-  const searchMatchBtnHandler = useEventHandler<QPushButtonSignals>(
-    {
-      clicked: () => {
-        lcuClientHandlerObj
-          .searchMatch()
-          .then(() => {})
-          .catch((err) =>
-            console.log("Error occured while trying to search the match", err)
-          );
-      },
-    },
-    []
-  );
+  function searchMatch() {
+    lcuClientHandlerObj
+      .searchMatch()
+      .then(() => {})
+      .catch((err) =>
+        console.log("Error occured while trying to search the match", err)
+      );
+  }
+
   return (
-    <Button style={playBtn} on={searchMatchBtnHandler}>
+    <Button
+      style={playBtn}
+      on={{
+        clicked: () => searchMatch(),
+      }}
+    >
       Search
     </Button>
   );
