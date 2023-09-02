96a89cd4f1b92ca813c39747fe959c35742f16a5
Darosss <daroing12@gmail.com>
2023-09-02 16:11:13 +0200

    refactor: remove pseudo-local-storage from index and import normally

--- src/lcu/index.ts
+++ src/lcu/index.ts
@@ -1,8 +1,4 @@
 export { LCUContext, LCUContextProvider } from "./lcu-context";
 export { lcuClientHandlerObj, LCUClientHandler } from "./lcu-client-handler";
-export {
-  readLocalStorageData,
-  writeLocalStorageData,
-} from "./pseudo-local-storage";
 export * from "./types";
 export { queues } from "./queues";
