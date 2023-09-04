6b7c2ac0c5ac56ca784d6ab44e9acf3cc4c84227
Darosss <daroing12@gmail.com>
2023-09-05 01:13:08 +0200

    style: add global, default styles variables

---
+++ src/styles/colors.ts
@@ -0,0 +1,26 @@
+import { backgroundLinearGradient } from "./styles-helpers";
+
+export const primaryLinearGradient = backgroundLinearGradient(
+  "rgba(25,0,36,0.6)",
+  "rgba(43,52,136,0.7)"
+);
+
+export const secondaryLinearGradient = backgroundLinearGradient(
+  `rgba(152, 162, 179, 0.8)`,
+  `rgba(76, 77, 79, 0.8)`
+);
+
+export const infoLinearGradient = backgroundLinearGradient(
+  `rgba(226, 244, 0, 0.8)`,
+  `rgba(161, 170, 49, 0.8)`
+);
+
+export const successLinearGradient = backgroundLinearGradient(
+  `rgba(35, 224, 37, 0.8)`,
+  `rgba(42, 119, 43, 0.8)`
+);
+
+export const dangerLinearGradient = backgroundLinearGradient(
+  `rgba(248, 42, 48, 0.8)`,
+  `rgba(189, 86, 64, 0.8)`
+);
