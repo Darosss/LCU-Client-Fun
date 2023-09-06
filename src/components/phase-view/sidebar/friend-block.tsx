fca76034d2164d3ff7a9f4d64725f0ecd85ad1b5
Darosss <daroing12@gmail.com>
2023-09-06 22:18:08 +0200

    feat: add info about info in game friends

--- src/components/phase-view/sidebar/friend-block.tsx
+++ src/components/phase-view/sidebar/friend-block.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { PrimaryText, SecondaryButton } from "@components";
+import { SecondaryButton, SecondaryText } from "@components";
 import { FriendsListData } from "@lcu";
 import { View } from "@nodegui/react-nodegui";
 import { FriendMenuActions } from "./friend-menu-actions";
@@ -9,15 +9,27 @@ interface FriendBlockProps {
 }
 export function FriendBlock({ friend }: FriendBlockProps) {
   const [showFriendMenu, setShowFriendMenu] = useState(false);
-
   return (
     <View id="friend-block-wrapper">
-      <SecondaryButton
-        text={friend.name}
-        on={{ clicked: () => setShowFriendMenu(!showFriendMenu) }}
-      />
-      {showFriendMenu ? <FriendMenuActions friend={friend} /> : null}
-      <PrimaryText text={friend.availability} />
+      <View>
+        <SecondaryButton
+          text={friend.name}
+          on={{ clicked: () => setShowFriendMenu(!showFriendMenu) }}
+        />
+        <SecondaryText text={friend.availability} />
+      </View>
+      {showFriendMenu ? (
+        <View>
+          <FriendMenuActions friend={friend} />
+        </View>
+      ) : null}
+      {friend.lol.gameStatus || friend.lol.gameQueueType ? (
+        <View>
+          <SecondaryText
+            text={`${friend.lol.gameStatus} ${friend.lol.gameQueueType}`}
+          />
+        </View>
+      ) : null}
     </View>
   );
 }
