84661020277ec58354e7245383b47a7934e775ca
Darosss <daroing12@gmail.com>
2023-09-08 13:59:21 +0200

    feat: add Cancel Search button

--- src/components/phase-view/matchmaking.tsx
+++ src/components/phase-view/matchmaking.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { View } from "@nodegui/react-nodegui";
-import { PrimaryText } from "@components";
+import { DangerButton, PrimaryText } from "@components";
 import { secondsToHMS } from "@helpers";
+import { LCUContext } from "@lcu";
 
 export function Matchmaking() {
+  const { lobbyLCUHandler } = useContext(LCUContext);
   const [searchingMatchTime, setSearchingMatchTime] = useState(0);
   useEffect(() => {
     const timer = setInterval(() => {
@@ -14,9 +16,21 @@ export function Matchmaking() {
       clearInterval(timer);
     };
   }, []);
+
   return (
     <View id="matchmaking-wrapper">
       <PrimaryText text={`Match making ${secondsToHMS(searchingMatchTime)}`} />
+      <DangerButton
+        text="Cancel search"
+        on={{
+          clicked: () =>
+            lobbyLCUHandler
+              ?.searchMatch(true)
+              .catch((err) =>
+                console.log(`Error occured while trying to cancel match`, err)
+              ),
+        }}
+      />
     </View>
   );
 }
