cd57287e2ba8f2bdb79baf02382defd5cc66c65a
Darosss <daroing12@gmail.com>
2023-09-21 00:28:18 +0200

    feat: add tabs lobby, champselect + in game/after game

--- src/components/phase-view/phaseview.tsx
+++ src/components/phase-view/phaseview.tsx
@@ -1,59 +1,104 @@
-import React, { useContext } from "react";
-import { View } from "@nodegui/react-nodegui";
+import React, { useContext, useEffect, useRef } from "react";
+import { TabItem, Tabs, View } from "@nodegui/react-nodegui";
 import { LCUContext } from "@lcu";
-import { ChampionSelectContextProvider, ChampSelect } from "@components";
+import {
+  ChampionSelectContextProvider,
+  ChampSelect,
+  Lobbys,
+} from "@components";
 import { ReadyCheck } from "./readycheck";
-import { MenuClient } from "./menu-client";
 import { Matchmaking } from "./matchmaking";
 import { WaitingForStats } from "./waiting-for-stats";
 import { Reconnect } from "./reconnect";
 import { GameStart } from "./game-start";
 import { InProgress } from "./in-progress";
 import { Sidebar } from "./sidebar";
-import { loggerWsEvents } from "../../logger";
+import { QTabWidget } from "@nodegui/nodegui";
+
+enum TabsIndex {
+  LOBBY,
+  CHAMP_SELECT,
+  IN_POST_GAME,
+}
+
 interface PhaseViewProps {}
 export function PhaseView({}: PhaseViewProps) {
   const { currentPhase } = useContext(LCUContext);
   console.log(new Date(), "PHASEVIEW => ", currentPhase);
-  loggerWsEvents.info(` PHASE => ${currentPhase}`);
-  function renderDependsOnPhase() {
+
+  const tabsRef = useRef<QTabWidget>(null);
+
+  useEffect(() => {
+    if (!tabsRef.current) return;
+    const { current } = tabsRef;
+
     switch (currentPhase) {
       case "ChampSelect":
-        return (
-          <ChampionSelectContextProvider>
-            <ChampSelect />
-          </ChampionSelectContextProvider>
-        );
-
-      case "InProgress":
-        return <InProgress />;
+        current.setCurrentIndex(TabsIndex.CHAMP_SELECT);
+        break;
 
       case "ReadyCheck":
-        return <ReadyCheck />;
-
       case "Matchmaking":
-        return <Matchmaking />;
+        current.setCurrentIndex(TabsIndex.LOBBY);
 
+        break;
+      case "InProgress":
       case "GameStart":
-        return <GameStart />;
-
       case "Reconnect":
-        return <Reconnect />;
-
       case "WaitingForStats":
-        return <WaitingForStats />;
-
-      case "None":
-      case "TerminatedInError":
-        return <></>;
+        current.setCurrentIndex(TabsIndex.IN_POST_GAME);
+        break;
     }
-  }
+
+    if (currentPhase === "ChampSelect") {
+      //FIXME: for now index 1 === champ select. Change to dynamical
+      current.setCurrentIndex(TabsIndex.CHAMP_SELECT);
+    } else if (
+      currentPhase === "ReadyCheck" ||
+      currentPhase === "Matchmaking"
+    ) {
+      current.setCurrentIndex(TabsIndex.LOBBY);
+    } else if (currentPhase === "InProgress" || currentPhase) {
+    }
+  }, [currentPhase]);
   return (
     <View>
       <View id="phases-wrapper">
         <View id="content-wrapper">
-          <MenuClient />
-          {renderDependsOnPhase()}
+          <Tabs ref={tabsRef}>
+            <TabItem title="lobby">
+              <View id="phase-view-tab-item-wrapper">
+                <Lobbys />
+                {currentPhase === "Matchmaking" ? (
+                  <Matchmaking />
+                ) : currentPhase === "ReadyCheck" ? (
+                  <ReadyCheck />
+                ) : null}
+              </View>
+            </TabItem>
+            <TabItem title="Champ select">
+              <View id="phase-view-tab-item-wrapper">
+                {currentPhase === "ChampSelect" ? (
+                  <ChampionSelectContextProvider>
+                    <ChampSelect />
+                  </ChampionSelectContextProvider>
+                ) : null}
+              </View>
+            </TabItem>
+            <TabItem title="In game / After game">
+              <View id="phase-view-tab-item-wrapper">
+                {currentPhase === "GameStart" ? (
+                  <GameStart />
+                ) : currentPhase === "InProgress" ? (
+                  <InProgress />
+                ) : currentPhase === "Reconnect" ? (
+                  <Reconnect />
+                ) : currentPhase === "WaitingForStats" ? (
+                  <WaitingForStats />
+                ) : null}
+              </View>
+            </TabItem>
+          </Tabs>
         </View>
         <View id="sidebar-wrapper">
           <Sidebar />
