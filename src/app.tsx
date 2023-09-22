3bc6a988e63611b56f19e341778f7bf4c0e5b859
Darosss <daroing12@gmail.com>
2023-09-23 00:37:38 +0200

    refactor: remove QIcon + windowIcon

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,15 @@
 import React, { useContext } from "react";
 import { Window, hot, View } from "@nodegui/react-nodegui";
-import { QIcon } from "@nodegui/nodegui";
-import nodeguiIcon from "@assets/nodegui.jpg";
 import { LCUContext, LCUContextProvider } from "@lcu";
 import { PhaseView } from "@components";
 import { appStylesheet } from "@styles";
 
-const winIcon = new QIcon(nodeguiIcon);
 function App() {
   const { options } = useContext(LCUContext);
 
   return (
     <LCUContextProvider>
       <Window
-        windowIcon={winIcon}
         windowTitle="LCU Client"
         minSize={options.minSize}
         maxSize={options.minSize}
@@ -31,5 +27,4 @@ function App() {
     </LCUContextProvider>
   );
 }
-
 export default hot(App);
