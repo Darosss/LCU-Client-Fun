ffa9e47198c32a560972441bb8e9b618fb5407cf
Darosss <daroing12@gmail.com>
2024-07-06 13:04:27 +0200

    feat(shared/helpers): add shuffleArrayRandomly fn

--- shared/helpers/arrays.helpers.ts
+++ shared/helpers/arrays.helpers.ts
@@ -1,3 +1,7 @@
 export function randomElement<T = unknown>(array: T[]) {
   return array[Math.floor(Math.random() * array.length)];
 }
+
+export function shuffleArrayRandomly<T = unknown>(providedArray: T[]) {
+  return providedArray.sort(() => 0.5 - Math.random());
+}
